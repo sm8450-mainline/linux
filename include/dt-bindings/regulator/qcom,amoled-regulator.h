4e02ce26b9dcedef59686885779d1c0813ca674a
Konrad Dybcio <konrad.dybcio@linaro.org>
2023-02-08 16:26:46 +0100

    dt-b: reg: amoled

---
+++ include/dt-bindings/regulator/qcom,amoled-regulator.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (c) 2023, Linaro Limited
+ */
+
+#ifndef __QCOM_AMOLED_REGULATOR_H
+#define __QCOM_AMOLED_REGULATOR_H
+
+/* Panel on */
+#define AMOLED_REGULATOR_MODE_ON	0
+/* Panel in LPn mode */
+#define AMOLED_REGULATOR_MODE_LPM	1
+/* Panel off */
+#define AMOLED_REGULATOR_MODE_OFF	2
+
+#endif
