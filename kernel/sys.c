57a6baf3a3ea61159a149573258a0a92f3a3d6dd
Marco Elver <elver@google.com>
2024-11-08 12:34:24 +0100

    tracing: Add task_prctl_unknown tracepoint
    prctl() is a complex syscall which multiplexes its functionality based
    on a large set of PR_* options. Currently we count 64 such options. The
    return value of unknown options is -EINVAL, and doesn't distinguish from
    known options that were passed invalid args that also return -EINVAL.
    
    To understand if programs are attempting to use prctl() options not yet
    available on the running kernel, provide the task_prctl_unknown
    tracepoint.
    
    Note, this tracepoint is in an unlikely cold path, and would therefore
    be suitable for continuous monitoring (e.g. via perf_event_open).
    
    While the above is likely the simplest usecase, additionally this
    tracepoint can help unlock some testing scenarios (where probing
    sys_enter or sys_exit causes undesirable performance overheads):
    
      a. unprivileged triggering of a test module: test modules may register a
         probe to be called back on task_prctl_unknown, and pick a very large
         unknown prctl() option upon which they perform a test function for an
         unprivileged user;
    
      b. unprivileged triggering of an eBPF program function: similar
         as idea (a).
    
    Example trace_pipe output:
    
      test-380     [001] .....    78.142904: task_prctl_unknown: option=1234 arg2=101 arg3=102 arg4=103 arg5=104
    
    Signed-off-by: Marco Elver <elver@google.com>
    Reviewed-by: Alexander Potapenko <glider@google.com>
    Link: https://lore.kernel.org/r/20241108113455.2924361-1-elver@google.com
    Signed-off-by: Kees Cook <kees@kernel.org>

--- kernel/sys.c
+++ kernel/sys.c
@@ -75,6 +75,8 @@
 #include <asm/io.h>
 #include <asm/unistd.h>
 
+#include <trace/events/task.h>
+
 #include "uid16.h"
 
 #ifndef SET_UNALIGN_CTL
@@ -2785,6 +2787,7 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 		error = RISCV_SET_ICACHE_FLUSH_CTX(arg2, arg3);
 		break;
 	default:
+		trace_task_prctl_unknown(option, arg2, arg3, arg4, arg5);
 		error = -EINVAL;
 		break;
 	}
